dbfa6e86730e8891af825322c3054692c3704aec
Brenden Wydra <brendenwwydra@gmail.com>
2023-11-13 10:16:15 -0600

    initial commit

--- ScheduleApp/ViewController.swift
+++ ScheduleApp/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var events = [Event]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func viewButtonPress(_ sender: UIButton) {
+        performSegue(withIdentifier: "toView", sender: self)
+    }
+    @IBAction func addButtonPress(_ sender: UIButton) {
+        performSegue(withIdentifier: "toAdd", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "toView") {
+            
+        }
+        if (segue.identifier == "toAdd"){
+            
+        }
+    }
+    
+}
 
+enum Day {
+    case monday, tuesday, wednesday, thursday, friday, saturday, sunday
+}
+
+struct Event {
+    var name: String
+    var color: UIColor
+    var day: Day
 }
 
---
+++ ScheduleApp/ViewControllerAdd.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerAdd.swift
+//  ScheduleApp
+//
+//  Created by BRENDEN WYDRA on 11/13/23.
+//
+
+import UIKit
+
+class ViewControllerAdd: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
